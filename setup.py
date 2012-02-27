4201fb4b974986ec2e12f6cf066cd1abc8fb3c03
Thomas Hunger <tehunger@gmail.com>
2012-02-27 19:55:25 +0000

    Move all parsed items to member variables.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from Cython.Distutils import build_ext
 sourcefiles = ['http11.pyx', 'http11_parser.c']
 
 setup(
+    name='http11',
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("http11", sourcefiles, extra_compile_args=["-g"])],
 )
--- testserver.py
+++ testserver.py
@@ -2,8 +2,41 @@ import socket
 import eventlet
 import http11
 
-def handle(sock):
-    p = http11.HttpParser()
+class InvalidRequest(Exception): pass
+
+def pipe(in_, out_):
+    while True:
+        d = in_.recv(2**10)
+        if d == '':
+            break
+        out_.sendall(d)
+
+def connect_backend(headers):
+    pass
+
+def handler(sock, parser, body_start):
+#    print parser.method, parser.uri, parser.fragment, parser.path, parser.query, parser.version
+    if parser.headers.get('expect') == '100-continue':
+        sock.sendall('HTTP/1.1 100 Continue\r\n\r\n')
+
+    # From here on we send a cleaned up request on to the correct
+    # client and, after establishing the connection, pipe
+    # bidirectionally.
+    backend_sock, response = connect_backend(headers)
+    backend_sock.sendall(body_start)
+
+    if len(body_start) < int(headers.get('content-length', 0)):
+        eventlet.spawn_n(pipe(sock, backend_sock))
+
+    pipe(backend_sock, sock)
+
+    try:
+        sock.shutdown(socket.SHUT_RDWR)
+    except socket.error:
+        pass
+
+def handle_internal(sock):
+    parser = http11.HttpParser()
     buf = bytearray(2**14)
     length, offset = 0, 0
 
@@ -13,20 +46,18 @@ def handle(sock):
         if not d:
             return
         buf[offset:length] = d
-        p.execute(buf, length)
-        offset = length
-        if not p.has_error() and p.is_finished():
+        parser.execute(buf, length)
+
+        if parser.has_error():
+            raise InvalidRequest()
+
+        if not parser.has_error() and parser.is_finished():
             break
-
-#    print p.request, p.headers
-    sock.sendall('HTTP/1.1 200 OK\r\nContent-length: 0\r\n\r\n')
-    try:
-        sock.shutdown(socket.SHUT_RDWR)
-    except socket.error:
-        pass
-
+        offset = length
+    body_start = buf[parser.body_start:length]
+    handler(sock, parser, body_start)
 
 l = eventlet.listen(('127.0.0.1', 5000), backlog=50)
 while True:
     sock, _ = l.accept()
-    handle(sock)
+    handle_internal(sock)

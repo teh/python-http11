071e032b3e6a32da6cdac026098752588b598250
Thomas Hunger <tehunger@gmail.com>
2012-02-27 22:29:26 +0000

    Example backend again.

--- testserver.py
+++ testserver.py
@@ -1,18 +1,27 @@
-import socket
 import eventlet
+from eventlet.green import socket
 import http11
 
 class InvalidRequest(Exception): pass
 
 def pipe(in_, out_):
     while True:
-        d = in_.recv(2**10)
+        d = in_.recv(2**13)
         if d == '':
             break
         out_.sendall(d)
 
-def connect_backend(headers):
-    pass
+
+def connect_backend(parser, backend_address):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(backend_address)
+    request = ['{} {} HTTP/1.0'.format(parser.method, parser.uri)]
+    for k, v in parser.headers.items():
+        request.append('{}: {}'.format(k, v))
+    request.append('\r\n')
+    sock.sendall('\r\n'.join(request))
+    return sock, ''
+
 
 def handler(sock, parser, body_start):
 #    print parser.method, parser.uri, parser.fragment, parser.path, parser.query, parser.version
@@ -22,18 +31,23 @@ def handler(sock, parser, body_start):
     # From here on we send a cleaned up request on to the correct
     # client and, after establishing the connection, pipe
     # bidirectionally.
-    backend_sock, response = connect_backend(headers)
-    backend_sock.sendall(body_start)
+    backend_sock, response = connect_backend(parser, ('127.0.0.1', 8000))
+    if body_start:
+        backend_sock.sendall(body_start)
 
-    if len(body_start) < int(headers.get('content-length', 0)):
+    if len(body_start) < int(parser.headers.get('content-length', 0)):
         eventlet.spawn_n(pipe(sock, backend_sock))
 
     pipe(backend_sock, sock)
-
+    try:
+        backend_sock.shutdown(socket.SHUT_RDWR)
+    except socket.error:
+        pass
     try:
         sock.shutdown(socket.SHUT_RDWR)
     except socket.error:
         pass
+
 
 def handle_internal(sock):
     parser = http11.HttpParser()
@@ -41,7 +55,7 @@ def handle_internal(sock):
     length, offset = 0, 0
 
     while True:
-        d = sock.recv(2**10)
+        d = sock.recv(2**12)
         length += len(d)
         if not d:
             return
@@ -60,4 +74,4 @@ def handle_internal(sock):
 l = eventlet.listen(('127.0.0.1', 5000), backlog=50)
 while True:
     sock, _ = l.accept()
-    handle_internal(sock)
+    eventlet.spawn_n(handle_internal, sock)

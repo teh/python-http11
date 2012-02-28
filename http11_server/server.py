e18dcc1ff72e58d00bfe31d74ea670a4102d0ee7
Thomas Hunger <tehunger@gmail.com>
2012-02-28 00:11:34 +0000

    Add server module

---
+++ http11_server/server.py
@@ -0,0 +1,79 @@
+import eventlet
+from eventlet.green import socket
+import http11
+
+class InvalidRequest(Exception): pass
+
+def pipe(in_, out_):
+    while True:
+        d = in_.recv(2**13)
+        if d == '':
+            break
+        out_.sendall(d)
+
+def connect_backend(sock, backend_sock, parser):
+    request = ['{} {} HTTP/1.0'.format(parser.method, parser.uri)]
+    for k, v in parser.headers.items():
+        request.append('{}: {}'.format(k, v))
+    request.append('\r\n')
+    backend_sock.sendall('\r\n'.join(request))
+
+def handle_rest(sock, backend_sock, parser, body_start):
+    if parser.headers.get('expect') == '100-continue':
+        sock.sendall('HTTP/1.1 100 Continue\r\n\r\n')
+
+    # From here on we send a cleaned up request on to the correct
+    # client and, after establishing the connection, pipe
+    # bidirectionally.
+    connect_backend(sock, backend_sock, parser)
+    if body_start:
+        backend_sock.sendall(body_start)
+
+    if len(body_start) < int(parser.headers.get('content-length', 0)):
+        eventlet.spawn_n(pipe(sock, backend_sock))
+
+    pipe(backend_sock, sock)
+    try:
+        backend_sock.shutdown(socket.SHUT_RDWR)
+    except socket.error:
+        pass
+    try:
+        sock.shutdown(socket.SHUT_RDWR)
+    except socket.error:
+        pass
+
+def handle_header(sock, from_):
+    parser = http11.HttpParser()
+    buf = bytearray(2**14)
+    length, offset = 0, 0
+
+    while True:
+        d = sock.recv(2**12)
+        length += len(d)
+        if not d:
+            return
+        buf[offset:length] = d
+        parser.execute(buf, length)
+
+        if parser.has_error():
+            raise InvalidRequest()
+
+        if not parser.has_error() and parser.is_finished():
+            break
+        offset = length
+    body_start = buf[parser.body_start:length]
+    parser.headers.update(remote=from_)
+    return parser, body_start
+
+def example_handler(sock, from_):
+    parser, body_start = handle_header(sock, from_)
+
+    backend_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    backend_sock.connect(('127.0.0.1', 8000))
+
+    handle_rest(sock, backend_sock, parser, body_start)
+
+def serve(listen_sock, handle):
+    while True:
+        sock, from_ = listen_sock.accept()
+        eventlet.spawn_n(handle, sock, from_)
--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ sourcefiles = ['http11.pyx', 'http11_parser.c']
 
 setup(
     name='http11',
+    version='0.1',
+    author='Tom',
+    author_email='tehunger@gmail.com',
+    packages=[
+        'http11_server',
+    ],
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("http11", sourcefiles, extra_compile_args=["-g"])],
 )
